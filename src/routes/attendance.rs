d7caf75b3ff9731f833dc15eecfe32e089a4a682
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-08 18:39:01 +0000

    add all routes

---
+++ src/routes/attendance.rs
@@ -0,0 +1,15 @@
+use axum::{
+    routing::{delete, get, post, put},
+    Router,
+};
+
+use crate::handlers::health::health_check;
+
+pub fn load_attendances_routes() -> Router {
+    let router = Router::new()
+        .route("/", get(health_check()))
+        .route("/", post(health_check()))
+        .route("/", delete(health_check()))
+        .route("/", put(health_check()));
+    router
+}
