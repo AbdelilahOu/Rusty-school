90d366f842b38c0dc1acf343e476285402ab9118
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-11 15:10:16 +0000

    change restful framwork

--- src/handlers/students.rs
+++ src/handlers/students.rs
@@ -1,7 +1,5 @@
-use axum::Json;
-
-pub async fn create_student(Json(payload): Json<String>) {}
-pub async fn update_student(Json(payload): Json<String>) {}
-pub async fn delete_student(Json(payload): Json<String>) {}
-pub async fn list_students(Json(payload): Json<String>) {}
-pub async fn get_student(Json(payload): Json<String>) {}
+pub async fn create_student() {}
+pub async fn update_student() {}
+pub async fn delete_student() {}
+pub async fn list_students() {}
+pub async fn get_student() {}
