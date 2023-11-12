862e753491241da5293cdde2675208d0476b0a8f
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-12 21:12:02 +0000

    try connection to database

---
+++ src/db/db.rs
@@ -0,0 +1,12 @@
+use dotenv::dotenv;
+use sea_orm::{Database, DatabaseConnection as DbConn, DbErr};
+
+pub async fn establish_connection() -> Result<DbConn, DbErr> {
+    dotenv().ok();
+    let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
+    let db_res = Database::connect(db_url).await;
+    match db_res {
+        Ok(db_conn) => Ok(db_conn),
+        Err(db_err) => Err(db_err),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
-use actix_web::{middleware::Logger, App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
+use db::db::establish_connection;
 use routes::students::load_students_routes;
+use sea_orm::DatabaseConnection;
 
+mod db;
 mod handlers;
 mod routes;
 
+struct AppState {
+    db_conn: DatabaseConnection,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .wrap(Logger::default())
-            .wrap(Logger::new("%a %{User-Agent}i"))
-            .service(load_students_routes())
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    let conn_res = establish_connection().await;
+    match conn_res {
+        Ok(conn) => {
+            let _ = HttpServer::new(|| {
+                App::new()
+                    // .app_data(web::Data::new(AppState { db_conn: conn }))
+                    .wrap(Logger::default())
+                    .wrap(Logger::new("%a %{User-Agent}i"))
+                    .service(load_students_routes())
+            })
+            .bind(("127.0.0.1", 8080))?
+            .run()
+            .await;
+        }
+        Err(db_err) => println!("{}", db_err.to_string()),
+    }
+    Ok(())
 }
