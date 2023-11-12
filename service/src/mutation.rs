484f13ce0d3335598fb2fea4e4ac5bbec2294e0e
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-12 20:49:07 +0000

    delete unused files

--- service/src/mutation.rs
+++ service/src/mutation.rs
@@ -1,10 +1,26 @@
 use ::entity::{student, student::Entity as Student};
-use sea_orm::*;
+use sea_orm::{prelude::Uuid, *};
 
 pub struct Mutation;
 
 impl Mutation {
     pub async fn create_student() -> Result<(), DbErr> {
+        Ok(())
+    }
+    pub async fn delete_student(db: &DbConn, id: Uuid) -> Result<u64, String> {
+        let student_res = Student::find_by_id(id).one(db).await;
+        match student_res {
+            Ok(student_opt) => match student_opt {
+                Some(student_a) => match student_a.delete(db).await {
+                    Ok(delete_a) => Ok(delete_a.rows_affected),
+                    Err(db_err) => Err(db_err.to_string()),
+                },
+                None => Err(String::from("student doesnt exist")),
+            },
+            Err(db_err) => Err(db_err.to_string()),
+        }
+    }
+    pub async fn update_student() -> Result<(), DbErr> {
         Ok(())
     }
 }
--- service/src/query.rs
+++ service/src/query.rs
@@ -1,10 +1,13 @@
 use ::entity::{student, student::Entity as Student};
-use sea_orm::*;
+use sea_orm::{prelude::Uuid, *};
 
 pub struct Query;
 
 impl Query {
-    pub async fn get_students() -> Result<(), DbErr> {
+    pub async fn list_students() -> Result<(), DbErr> {
+        Ok(())
+    }
+    pub async fn get_student(_id: Uuid) -> Result<(), DbErr> {
         Ok(())
     }
 }
