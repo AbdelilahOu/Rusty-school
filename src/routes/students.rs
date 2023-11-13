80d75defbe60eed0fac07ec3e673ab9289ab13d8
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-13 08:32:08 +0000

    use right http method

--- src/routes/students.rs
+++ src/routes/students.rs
@@ -3,6 +3,6 @@ use actix_web::{web, Scope};
 
 pub fn load_students_routes() -> Scope {
     web::scope("/students")
-        .route("/", web::get().to(students::create_student))
+        .route("/", web::post().to(students::create_student))
         .route("/{id}", web::delete().to(students::delete_student))
 }
