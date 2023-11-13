37f150a9231167cdf52382d7e7c0bb367e047236
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-13 15:54:48 +0000

    add db connection options

--- src/db/db.rs
+++ src/db/db.rs
@@ -1,10 +1,20 @@
 use dotenv::dotenv;
-use sea_orm::{Database, DatabaseConnection as DbConn, DbErr};
+use sea_orm::{ConnectOptions, Database, DatabaseConnection as DbConn, DbErr};
+use std::time::Duration;
 
 pub async fn establish_connection() -> Result<DbConn, DbErr> {
     dotenv().ok();
+    // get database url from env
     let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
-    let db_res = Database::connect(db_url).await;
+    // init connection options
+    let mut options = ConnectOptions::new(db_url);
+    options
+        .max_connections(3)
+        .min_connections(1)
+        .connect_timeout(Duration::from_secs(5));
+    // get db connection
+    let db_res = Database::connect(options).await;
+    // check for errors
     match db_res {
         Ok(db_conn) => {
             println!("Database connection established.");
