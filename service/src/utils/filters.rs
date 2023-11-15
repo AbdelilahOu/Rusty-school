28af757f12efe070cd87d8cd3d296b5604006af6
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-15 13:49:50 +0000

    update filters

--- service/src/utils/filters.rs
+++ service/src/utils/filters.rs
@@ -7,9 +7,27 @@ pub fn generate_student_filters(filters: Vec<Filters>) -> Condition {
         .into_iter()
         .fold(Condition::all(), |conditions, filter| {
             match filter.feild.as_str() {
-                "full_name" => conditions.add(Column::FirstName.like(filter.value)),
-                "last_name" => conditions.add(Column::LastName.like(filter.value)),
-                "first_name" => conditions.add(Column::FirstName.like(filter.value)),
+                "full_name" => match filter.operation.as_str() {
+                    "LIKE" => conditions.add(Column::FullName.like(filter.value)),
+                    "CONTAINS" => conditions.add(Column::FullName.contains(filter.value)),
+                    "STARTS_WITH" => conditions.add(Column::FullName.starts_with(filter.value)),
+                    "ENDS_WITH" => conditions.add(Column::FullName.ends_with(filter.value)),
+                    _ => conditions,
+                },
+                "last_name" => match filter.operation.as_str() {
+                    "LIKE" => conditions.add(Column::LastName.like(filter.value)),
+                    "CONTAINS" => conditions.add(Column::LastName.contains(filter.value)),
+                    "STARTS_WITH" => conditions.add(Column::LastName.starts_with(filter.value)),
+                    "ENDS_WITH" => conditions.add(Column::LastName.ends_with(filter.value)),
+                    _ => conditions,
+                },
+                "first_name" => match filter.operation.as_str() {
+                    "LIKE" => conditions.add(Column::FirstName.like(filter.value)),
+                    "CONTAINS" => conditions.add(Column::FirstName.contains(filter.value)),
+                    "STARTS_WITH" => conditions.add(Column::FirstName.starts_with(filter.value)),
+                    "ENDS_WITH" => conditions.add(Column::FirstName.ends_with(filter.value)),
+                    _ => conditions,
+                },
                 "level" => conditions,
                 "address" => conditions,
                 _ => conditions,
