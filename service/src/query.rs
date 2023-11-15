2d16ea786ef516dc4922f86ef4e23ab3201198ad
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-15 14:09:36 +0000

    add teachers query

--- service/src/query.rs
+++ service/src/query.rs
@@ -1,5 +1,8 @@
-use crate::{generate_student_filters, GStudent, QueriesFilters};
+use crate::{
+    generate_student_filters, generate_teacher_filters, GStudent, GTeacher, QueriesFilters,
+};
 use ::entity::students::Entity as Student;
+use ::entity::teachers::Entity as Teacher;
 use sea_orm::{prelude::Uuid, *};
 
 pub struct ServiceQuery;
@@ -45,4 +48,38 @@ impl ServiceQuery {
         }
     }
     //
+    pub async fn list_teachers(qf: QueriesFilters, db: &DbConn) -> Result<Vec<GTeacher>, String> {
+        let list_teachers = Teacher::find()
+            .offset((qf.queries.page - 1) * qf.queries.limit)
+            .limit(qf.queries.limit)
+            .filter(generate_teacher_filters(qf.filters))
+            .all(db)
+            .await;
+
+        match list_teachers {
+            Ok(teachers) => {
+                let maped_teachers = teachers.into_iter().map(|teacher| GTeacher {
+                    id: teacher.id,
+                    first_name: teacher.first_name,
+                    last_name: teacher.last_name,
+                });
+                Ok(maped_teachers.collect())
+            }
+            Err(err) => Err(err.to_string()),
+        }
+    }
+    pub async fn get_teacher(id: Uuid, db: &DbConn) -> Result<GTeacher, String> {
+        let selected_teacher = Teacher::find_by_id(id).one(db).await;
+        match selected_teacher {
+            Ok(teacher_op) => match teacher_op {
+                Some(some_teacher) => Ok(GTeacher {
+                    id: some_teacher.id,
+                    first_name: some_teacher.first_name,
+                    last_name: some_teacher.last_name,
+                }),
+                None => Err(String::from("teacher doesnt exist")),
+            },
+            Err(e) => Err(e.to_string()),
+        }
+    }
 }
