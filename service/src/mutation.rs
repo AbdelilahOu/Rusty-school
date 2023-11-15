28ea8c91cb8a51281bbc6a8291a799ac981003d2
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-15 14:09:29 +0000

    add teachers mut

--- service/src/mutation.rs
+++ service/src/mutation.rs
@@ -1,14 +1,15 @@
-use ::entity::students::{ActiveModel, Entity as Student};
+use ::entity::students::{ActiveModel as StudentActiveModel, Entity as Student};
+use ::entity::teachers::{ActiveModel as TeacherActiveModel, Entity as Teacher};
 use sea_orm::{prelude::Uuid, *};
 
-use crate::CStudent;
+use crate::{CStudent, CTeacher};
 
 pub struct ServiceMutation;
 
 impl ServiceMutation {
     // students entity
     pub async fn create_student(db: &DbConn, data: CStudent) -> Result<Uuid, DbErr> {
-        let c_student = ActiveModel {
+        let c_student = StudentActiveModel {
             first_name: Set(data.first_name),
             last_name: Set(data.last_name),
             address: Set(data.address),
@@ -38,7 +39,7 @@ impl ServiceMutation {
         match selected_student {
             Ok(student_opt) => match student_opt {
                 Some(student_model) => {
-                    let mut student_model: ActiveModel = student_model.into();
+                    let mut student_model: StudentActiveModel = student_model.into();
                     student_model.first_name = Set(data.first_name);
                     student_model.last_name = Set(data.last_name);
                     student_model.address = Set(data.address);
@@ -60,4 +61,50 @@ impl ServiceMutation {
         }
     }
     // teachers entity
+    pub async fn create_teacher(db: &DbConn, data: CTeacher) -> Result<Uuid, DbErr> {
+        let c_teacher = TeacherActiveModel {
+            first_name: Set(data.first_name),
+            last_name: Set(data.last_name),
+            ..Default::default()
+        };
+        match Teacher::insert(c_teacher).exec(db).await {
+            Ok(res) => Ok(res.last_insert_id),
+            Err(err) => return Err(err),
+        }
+    }
+    pub async fn delete_teacher(db: &DbConn, id: Uuid) -> Result<u64, String> {
+        let selected_teacher = Teacher::find_by_id(id).one(db).await;
+        match selected_teacher {
+            Ok(teacher_opt) => match teacher_opt {
+                Some(teacher_model) => match teacher_model.delete(db).await {
+                    Ok(delete_a) => Ok(delete_a.rows_affected),
+                    Err(err) => Err(err.to_string()),
+                },
+                None => Err(String::from("teacher doesnt exist")),
+            },
+            Err(err) => Err(err.to_string()),
+        }
+    }
+    pub async fn update_teacher(db: &DbConn, id: Uuid, data: CTeacher) -> Result<CTeacher, String> {
+        let selected_teacher = Teacher::find_by_id(id).one(db).await;
+        match selected_teacher {
+            Ok(teacher_opt) => match teacher_opt {
+                Some(teacher_model) => {
+                    let mut teacher_model: TeacherActiveModel = teacher_model.into();
+                    teacher_model.first_name = Set(data.first_name);
+                    teacher_model.last_name = Set(data.last_name);
+                    teacher_model.id = Set(id);
+                    match teacher_model.update(db).await {
+                        Ok(updated_teacher) => Ok(CTeacher {
+                            first_name: updated_teacher.first_name,
+                            last_name: updated_teacher.last_name,
+                        }),
+                        Err(err) => Err(err.to_string()),
+                    }
+                }
+                None => Err(String::from("teacher doesnt exist")),
+            },
+            Err(err) => Err(err.to_string()),
+        }
+    }
 }
