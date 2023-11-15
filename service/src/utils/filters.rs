3cd3a4fa2463ef9424d97025b435fa9dac2eabeb
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-15 11:04:28 +0000

    filters works

--- service/src/utils/filters.rs
+++ service/src/utils/filters.rs
@@ -1,12 +1,16 @@
 use crate::Filters;
-use sea_orm::Condition;
+use entity::students;
+use sea_orm::{ColumnTrait, Condition};
 
 pub fn generate_student_filters(filters: Vec<Filters>) -> Condition {
-    let conditions = Condition::all();
-    for filter in filters {
-        match filter.feild {
-            _ => {}
-        }
-    }
-    conditions
+    filters
+        .into_iter()
+        .fold(Condition::all(), |conditions, filter| {
+            match filter.feild.as_str() {
+                "full_name" => conditions.add(students::Column::FirstName.like(filter.value)),
+                "level" => conditions,
+                "address" => conditions,
+                _ => conditions,
+            }
+        })
 }
