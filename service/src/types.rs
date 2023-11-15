fab8187c97b6d947bcaec5924a69089625f64472
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-15 14:09:43 +0000

    update types

--- service/src/types.rs
+++ service/src/types.rs
@@ -1,6 +1,24 @@
 use sea_orm::prelude::Uuid;
 use serde::{Deserialize, Serialize};
 
+#[derive(Deserialize)]
+pub struct ListQuery {
+    pub page: u64,
+    pub limit: u64,
+}
+
+#[derive(Deserialize, Clone)]
+pub struct Filters {
+    pub feild: String,
+    pub operation: String,
+    pub value: String,
+}
+
+pub struct QueriesFilters {
+    pub queries: ListQuery,
+    pub filters: Vec<Filters>,
+}
+
 #[derive(Deserialize, Serialize, Debug)]
 pub struct CStudent {
     pub first_name: String,
@@ -18,20 +36,15 @@ pub struct GStudent {
     pub level: String,
 }
 
-#[derive(Deserialize)]
-pub struct ListQuery {
-    pub page: u64,
-    pub limit: u64,
+#[derive(Deserialize, Serialize, Debug)]
+pub struct CTeacher {
+    pub first_name: String,
+    pub last_name: String,
 }
 
-#[derive(Deserialize, Clone)]
-pub struct Filters {
-    pub feild: String,
-    pub operation: String,
-    pub value: String,
-}
-
-pub struct QueriesFilters {
-    pub queries: ListQuery,
-    pub filters: Vec<Filters>,
+#[derive(Serialize, Debug)]
+pub struct GTeacher {
+    pub id: Uuid,
+    pub first_name: String,
+    pub last_name: String,
 }
