46bfe47f3c00adc223e875150a4fbbee28dd889e
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-16 21:51:58 +0000

    add scans table

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -4,6 +4,7 @@ mod m20231109_190937_c_student;
 mod m20231113_170500_c_teacher;
 mod m20231116_165911_c_parents;
 mod m20231116_171406_c_pickups;
+mod m20231116_214011_c_scans;
 
 pub struct Migrator;
 
@@ -15,6 +16,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20231113_170500_c_teacher::Migration),
             Box::new(m20231116_165911_c_parents::Migration),
             Box::new(m20231116_171406_c_pickups::Migration),
+            Box::new(m20231116_214011_c_scans::Migration),
         ]
     }
 }
