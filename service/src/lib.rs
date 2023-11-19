285e7b8c9cb2af1e56a53daa384ca99c97e59a10
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-19 09:32:29 +0000

    add transactions

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -1,10 +1,12 @@
 mod mutation;
 mod query;
+mod transaction;
 mod types;
 mod utils;
 
 pub use mutation::*;
 pub use query::*;
+pub use transaction::*;
 pub use types::*;
 pub use utils::filters::*;
 
---
+++ service/src/transaction.rs
@@ -0,0 +1,3 @@
+pub struct ServiceTransaction;
+
+impl ServiceTransaction {}
