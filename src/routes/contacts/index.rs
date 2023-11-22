1eb7416999e82e4100050a2336a31f0e46e1e058
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-22 20:55:57 +0000

    rearrange code

--- src/routes/contacts/index.rs
+++ src/routes/contacts/index.rs
@@ -2,10 +2,7 @@ use actix_web::{web, Scope};
 
 use crate::handlers::contacts;
 
-use super::{
-    cities::load_cities_routes, countries::load_countries_routes, districts::load_districts_routes,
-    states::load_states_routes, streets::load_streets_routes,
-};
+use super::{cities::*, countries::*, districts::*, states::*, streets::*};
 
 pub fn load_contacts_routes() -> Scope {
     web::scope("/contacts")
@@ -14,8 +11,8 @@ pub fn load_contacts_routes() -> Scope {
         .route("/{id}", web::put().to(contacts::update_contact))
         .route("/{id}", web::delete().to(contacts::delete_contact))
         .service(load_countries_routes())
-        .service(load_states_routes())
-        .service(load_cities_routes())
         .service(load_districts_routes())
         .service(load_streets_routes())
+        .service(load_states_routes())
+        .service(load_cities_routes())
 }
