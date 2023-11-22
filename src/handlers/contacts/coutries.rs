81d52650608742d60635e94f87487941761c4643
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-22 06:28:22 +0000

    add sub handlers

--- src/handlers/contacts/coutries.rs
+++ src/handlers/contacts/coutries.rs
@@ -10,10 +10,18 @@ use uuid::Uuid;
 
 type State = ActData<AppState>;
 
-pub async fn create_country(state: State) -> HttpResponse {}
+pub async fn create_country(state: State) -> HttpResponse {
+    HttpResponse::Ok().status(StatusCode::OK).body("great")
+}
 
-pub async fn get_country(state: State) -> HttpResponse {}
+pub async fn get_country(state: State) -> HttpResponse {
+    HttpResponse::Ok().status(StatusCode::OK).body("great")
+}
 
-pub async fn update_country(state: State) -> HttpResponse {}
+pub async fn update_country(state: State) -> HttpResponse {
+    HttpResponse::Ok().status(StatusCode::OK).body("great")
+}
 
-pub async fn delete_country(state: State) -> HttpResponse {}
+pub async fn delete_country(state: State) -> HttpResponse {
+    HttpResponse::Ok().status(StatusCode::OK).body("great")
+}
