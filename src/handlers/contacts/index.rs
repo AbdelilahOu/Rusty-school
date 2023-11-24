de7458d76aac4cd1c8ec6c02821a972227ea2313
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-24 08:45:57 +0000

    add contact

--- src/handlers/contacts/index.rs
+++ src/handlers/contacts/index.rs
@@ -1,27 +1,79 @@
 use crate::{models::commen::*, AppState};
 use actix_web::{
     http::{header::ContentType, StatusCode},
-    web::{Data as ActData, Json as ActJson, Path as ActPath, Query as ActQuery},
+    web::{Data as ActData, Json as ActJson, Path as ActPath},
     HttpResponse,
 };
 
 use service::*;
 use uuid::Uuid;
 
+// i like my functions to stay inline
+// type TFiltersBody = ActJson<FiltersBody>;
 type State = ActData<AppState>;
+type IdParam = ActPath<Uuid>;
+type CtBody = ActJson<CContact>;
 
-pub async fn create_contact(state: State) -> HttpResponse {
-    HttpResponse::Ok().status(StatusCode::OK).body("great")
+pub async fn create_contact(body: CtBody, state: State) -> HttpResponse {
+    let res = ServiceMutation::create_contact(&state.db_conn, body.into_inner()).await;
+    match res {
+        Ok(id) => HttpResponse::Ok()
+            .status(StatusCode::CREATED)
+            .content_type(ContentType::json())
+            .json(ResultResponse {
+                error: None,
+                message: Some("State created successfully".to_string()),
+                data: Some(id.to_string()),
+            }),
+        Err(e) => HttpResponse::Ok()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .content_type(ContentType::json())
+            .json(ResultResponse::<Option<String>> {
+                error: Some(e.to_string()),
+                message: None,
+                data: None,
+            }),
+    }
 }
 
-pub async fn get_contact(state: State) -> HttpResponse {
-    HttpResponse::Ok().status(StatusCode::OK).body("great")
+pub async fn delete_contact(id: IdParam, state: State) -> HttpResponse {
+    let delete_res = ServiceMutation::delete_contact(&state.db_conn, id.into_inner()).await;
+
+    match delete_res {
+        Ok(i) => HttpResponse::Created()
+            .content_type(ContentType::json())
+            .json(ResultResponse {
+                error: None,
+                message: Some("State deleted successfully".to_string()),
+                data: Some(i.to_string()),
+            }),
+        Err(e) => HttpResponse::InternalServerError()
+            .content_type(ContentType::json())
+            .json(ResultResponse::<Option<String>> {
+                error: Some(e),
+                message: None,
+                data: None,
+            }),
+    }
 }
 
-pub async fn update_contact(state: State) -> HttpResponse {
-    HttpResponse::Ok().status(StatusCode::OK).body("great")
-}
-
-pub async fn delete_contact(state: State) -> HttpResponse {
-    HttpResponse::Ok().status(StatusCode::OK).body("great")
+pub async fn update_contact(id: IdParam, body: CtBody, state: State) -> HttpResponse {
+    let update_res =
+        ServiceMutation::update_contact(&state.db_conn, id.into_inner(), body.into_inner()).await;
+    match update_res {
+        Ok(i) => HttpResponse::Created()
+            .content_type(ContentType::json())
+            .json(ResultResponse {
+                error: None,
+                message: Some("State updated successfully".to_string()),
+                data: Some(i),
+            }),
+        Err(e) => HttpResponse::InternalServerError()
+            .content_type(ContentType::json())
+            .json(ResultResponse::<Option<String>> {
+                error: Some(e),
+                message: None,
+                data: None,
+            }),
+    }
 }
--- src/routes/contacts/cities.rs
+++ src/routes/contacts/cities.rs
@@ -4,7 +4,7 @@ use crate::handlers::contacts;
 
 pub fn load_cities_routes() -> Scope {
     web::scope("/cities")
-        .route("/", web::get().to(contacts::get_city))
+        // .route("/", web::get().to(contacts::get_city))
         .route("/", web::post().to(contacts::create_city))
         .route("/{id}", web::put().to(contacts::update_city))
         .route("/{id}", web::delete().to(contacts::delete_city))
