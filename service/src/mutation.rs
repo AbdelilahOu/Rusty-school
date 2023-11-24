1f424f8d184547a539e372d58760209c9c8063ff
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-11-24 07:31:58 +0000

    finish crud

--- service/src/mutation.rs
+++ service/src/mutation.rs
@@ -1,19 +1,16 @@
-use ::entity::cities::{ActiveModel as CityActiveModel, Entity as City};
-use ::entity::contacts_informations::{ActiveModel as ContactActiveModel, Entity as Contact};
-use ::entity::countries::{ActiveModel as CountryActiveModel, Entity as Country};
-use ::entity::parents::{ActiveModel as ParentActiveModel, Entity as Parent};
-use ::entity::states::{ActiveModel as StateActiveModel, Entity as State};
-use ::entity::students::{ActiveModel as StudentActiveModel, Entity as Student};
-use ::entity::teachers::{ActiveModel as TeacherActiveModel, Entity as Teacher};
+use ::entity::prelude::*;
 use sea_orm::{prelude::Uuid, *};
 
-use crate::{CCity, CContact, CCountry, CParent, CState, CStudent, CTeacher};
+use super::types::*;
 
 pub struct ServiceMutation;
 
+type CResult<T> = Result<T, DbErr>;
+type GResult<T> = Result<T, String>;
+
 impl ServiceMutation {
     // students entity
-    pub async fn create_student(db: &DbConn, data: CStudent) -> Result<Uuid, DbErr> {
+    pub async fn create_student(db: &DbConn, data: CStudent) -> CResult<Uuid> {
         let c_student = StudentActiveModel {
             first_name: Set(data.first_name),
             last_name: Set(data.last_name),
@@ -25,7 +22,7 @@ impl ServiceMutation {
             Err(err) => return Err(err),
         }
     }
-    pub async fn delete_student(db: &DbConn, id: Uuid) -> Result<u64, String> {
+    pub async fn delete_student(db: &DbConn, id: Uuid) -> GResult<u64> {
         let selected_student = Student::find_by_id(id).one(db).await;
         match selected_student {
             Ok(student_opt) => match student_opt {
@@ -38,7 +35,7 @@ impl ServiceMutation {
             Err(err) => Err(err.to_string()),
         }
     }
-    pub async fn update_student(db: &DbConn, id: Uuid, data: CStudent) -> Result<CStudent, String> {
+    pub async fn update_student(db: &DbConn, id: Uuid, data: CStudent) -> GResult<CStudent> {
         let selected_student = Student::find_by_id(id).one(db).await;
         match selected_student {
             Ok(student_opt) => match student_opt {
@@ -63,7 +60,7 @@ impl ServiceMutation {
         }
     }
     // teachers entity
-    pub async fn create_teacher(db: &DbConn, data: CTeacher) -> Result<Uuid, DbErr> {
+    pub async fn create_teacher(db: &DbConn, data: CTeacher) -> CResult<Uuid> {
         let c_teacher = TeacherActiveModel {
             first_name: Set(data.first_name),
             last_name: Set(data.last_name),
@@ -74,7 +71,7 @@ impl ServiceMutation {
             Err(err) => return Err(err),
         }
     }
-    pub async fn delete_teacher(db: &DbConn, id: Uuid) -> Result<u64, String> {
+    pub async fn delete_teacher(db: &DbConn, id: Uuid) -> GResult<u64> {
         let selected_teacher = Teacher::find_by_id(id).one(db).await;
         match selected_teacher {
             Ok(teacher_opt) => match teacher_opt {
@@ -87,7 +84,7 @@ impl ServiceMutation {
             Err(err) => Err(err.to_string()),
         }
     }
-    pub async fn update_teacher(db: &DbConn, id: Uuid, data: CTeacher) -> Result<CTeacher, String> {
+    pub async fn update_teacher(db: &DbConn, id: Uuid, data: CTeacher) -> GResult<CTeacher> {
         let selected_teacher = Teacher::find_by_id(id).one(db).await;
         match selected_teacher {
             Ok(teacher_opt) => match teacher_opt {
@@ -110,7 +107,7 @@ impl ServiceMutation {
         }
     }
     // parents entity
-    pub async fn create_parent(db: &DbConn, data: CParent) -> Result<Uuid, DbErr> {
+    pub async fn create_parent(db: &DbConn, data: CParent) -> CResult<Uuid> {
         let c_parent = ParentActiveModel {
             first_name: Set(data.first_name),
             last_name: Set(data.last_name),
@@ -121,7 +118,7 @@ impl ServiceMutation {
             Err(err) => return Err(err),
         }
     }
-    pub async fn delete_parent(db: &DbConn, id: Uuid) -> Result<u64, String> {
+    pub async fn delete_parent(db: &DbConn, id: Uuid) -> GResult<u64> {
         let selected_parent = Parent::find_by_id(id).one(db).await;
         match selected_parent {
             Ok(parent_opt) => match parent_opt {
@@ -134,7 +131,7 @@ impl ServiceMutation {
             Err(err) => Err(err.to_string()),
         }
     }
-    pub async fn update_parent(db: &DbConn, id: Uuid, data: CParent) -> Result<CParent, String> {
+    pub async fn update_parent(db: &DbConn, id: Uuid, data: CParent) -> GResult<CParent> {
         let selected_parent = Parent::find_by_id(id).one(db).await;
         match selected_parent {
             Ok(parent_opt) => match parent_opt {
@@ -157,7 +154,7 @@ impl ServiceMutation {
         }
     }
     // contacts
-    pub async fn create_contact(db: &DbConn, data: CContact) -> Result<Uuid, DbErr> {
+    pub async fn create_contact(db: &DbConn, data: CContact) -> CResult<Uuid> {
         let c_contact = ContactActiveModel {
             phone_number: Set(data.phone),
             email: Set(data.email),
@@ -173,7 +170,7 @@ impl ServiceMutation {
             Err(err) => return Err(err),
         }
     }
-    pub async fn delete_contact(db: &DbConn, id: Uuid) -> Result<u64, String> {
+    pub async fn delete_contact(db: &DbConn, id: Uuid) -> GResult<u64> {
         let selected_contact = Contact::find_by_id(id).one(db).await;
         match selected_contact {
             Ok(contact_opt) => match contact_opt {
@@ -186,7 +183,7 @@ impl ServiceMutation {
             Err(err) => Err(err.to_string()),
         }
     }
-    pub async fn update_contact(db: &DbConn, id: Uuid, data: CContact) -> Result<CContact, String> {
+    pub async fn update_contact(db: &DbConn, id: Uuid, data: CContact) -> GResult<CContact> {
         let selected_contact = Contact::find_by_id(id).one(db).await;
         match selected_contact {
             Ok(contact_opt) => match contact_opt {
@@ -219,7 +216,7 @@ impl ServiceMutation {
         }
     }
     // country
-    pub async fn create_country(db: &DbConn, data: CCountry) -> Result<Uuid, DbErr> {
+    pub async fn create_country(db: &DbConn, data: CCountry) -> CResult<Uuid> {
         let c_country = CountryActiveModel {
             country_name: Set(data.name),
             country_initials: Set(data.initials),
@@ -230,7 +227,7 @@ impl ServiceMutation {
             Err(err) => return Err(err),
         }
     }
-    pub async fn delete_country(db: &DbConn, id: Uuid) -> Result<u64, String> {
+    pub async fn delete_country(db: &DbConn, id: Uuid) -> GResult<u64> {
         let selected_country = Country::find_by_id(id).one(db).await;
         match selected_country {
             Ok(country_opt) => match country_opt {
@@ -243,7 +240,7 @@ impl ServiceMutation {
             Err(err) => Err(err.to_string()),
         }
     }
-    pub async fn update_country(db: &DbConn, id: Uuid, data: CCountry) -> Result<CCountry, String> {
+    pub async fn update_country(db: &DbConn, id: Uuid, data: CCountry) -> GResult<CCountry> {
         let selected_country = Country::find_by_id(id).one(db).await;
         match selected_country {
             Ok(country_opt) => match country_opt {
@@ -264,8 +261,107 @@ impl ServiceMutation {
             Err(e) => Err(e.to_string()),
         }
     }
+    // districts
+    pub async fn create_district(db: &DbConn, data: CDistrict) -> CResult<Uuid> {
+        let c_district = DistrictActiveModel {
+            district_name: Set(data.name),
+            city_id: Set(data.city_id),
+            ..Default::default()
+        };
+        match District::insert(c_district).exec(db).await {
+            Ok(res) => Ok(res.last_insert_id),
+            Err(err) => return Err(err),
+        }
+    }
+    pub async fn delete_district(db: &DbConn, id: Uuid) -> GResult<u64> {
+        let selected_district = District::find_by_id(id).one(db).await;
+        match selected_district {
+            Ok(district_opt) => match district_opt {
+                Some(district_model) => match district_model.delete(db).await {
+                    Ok(delete_a) => Ok(delete_a.rows_affected),
+                    Err(err) => Err(err.to_string()),
+                },
+                None => Err(String::from("district doesnt exist")),
+            },
+            Err(err) => Err(err.to_string()),
+        }
+    }
+    pub async fn update_district(db: &DbConn, id: Uuid, data: CDistrict) -> GResult<CDistrict> {
+        let selected_district = District::find_by_id(id).one(db).await;
+        match selected_district {
+            Ok(district_opt) => match district_opt {
+                Some(district_model) => {
+                    let mut district_model: DistrictActiveModel = district_model.into();
+                    district_model.district_name = Set(data.name);
+                    district_model.city_id = Set(data.city_id);
+                    match district_model.update(db).await {
+                        Ok(updated_district) => Ok(CDistrict {
+                            name: updated_district.district_name,
+                            city_id: updated_district.city_id,
+                        }),
+                        Err(e) => Err(e.to_string()),
+                    }
+                }
+                None => Err(String::from("district doesnt exist")),
+            },
+            Err(e) => Err(e.to_string()),
+        }
+    }
+    // steets
+    pub async fn create_street(db: &DbConn, data: CStreet) -> CResult<Uuid> {
+        let c_street = StreetActiveModel {
+            street_name: Set(data.name),
+            district_id: Set(data.district_id),
+            street_type: Set(data.street_type),
+            zip_code: Set(data.zip_code),
+            ..Default::default()
+        };
+        match Street::insert(c_street).exec(db).await {
+            Ok(res) => Ok(res.last_insert_id),
+            Err(err) => return Err(err),
+        }
+    }
+    pub async fn delete_street(db: &DbConn, id: Uuid) -> GResult<u64> {
+        let selected_street = Street::find_by_id(id).one(db).await;
+        match selected_street {
+            Ok(street_opt) => match street_opt {
+                Some(street_model) => match street_model.delete(db).await {
+                    Ok(delete_a) => Ok(delete_a.rows_affected),
+                    Err(err) => Err(err.to_string()),
+                },
+                None => Err(String::from("street doesnt exist")),
+            },
+            Err(err) => Err(err.to_string()),
+        }
+    }
+    pub async fn update_street(db: &DbConn, id: Uuid, data: CStreet) -> GResult<CStreet> {
+        let selected_street = Street::find_by_id(id).one(db).await;
+        match selected_street {
+            Ok(street_opt) => match street_opt {
+                Some(street_model) => {
+                    let mut street_model: StreetActiveModel = street_model.into();
+                    street_model.street_name = Set(data.name);
+                    street_model.district_id = Set(data.district_id);
+                    street_model.street_type = Set(data.street_type);
+                    street_model.zip_code = Set(data.zip_code);
+
+                    match street_model.update(db).await {
+                        Ok(updated_street) => Ok(CStreet {
+                            name: updated_street.street_name,
+                            district_id: updated_street.district_id,
+                            street_type: updated_street.street_type,
+                            zip_code: updated_street.zip_code,
+                        }),
+                        Err(e) => Err(e.to_string()),
+                    }
+                }
+                None => Err(String::from("street doesnt exist")),
+            },
+            Err(e) => Err(e.to_string()),
+        }
+    }
     // state
-    pub async fn create_state(db: &DbConn, data: CState) -> Result<Uuid, DbErr> {
+    pub async fn create_state(db: &DbConn, data: CState) -> CResult<Uuid> {
         let c_state = StateActiveModel {
             state_name: Set(data.name),
             state_initials: Set(data.initials),
@@ -278,7 +374,7 @@ impl ServiceMutation {
             Err(err) => return Err(err),
         }
     }
-    pub async fn delete_state(db: &DbConn, id: Uuid) -> Result<u64, String> {
+    pub async fn delete_state(db: &DbConn, id: Uuid) -> GResult<u64> {
         let selected_state = State::find_by_id(id).one(db).await;
         match selected_state {
             Ok(state_opt) => match state_opt {
@@ -291,7 +387,7 @@ impl ServiceMutation {
             Err(err) => Err(err.to_string()),
         }
     }
-    pub async fn update_state(db: &DbConn, id: Uuid, data: CState) -> Result<CState, String> {
+    pub async fn update_state(db: &DbConn, id: Uuid, data: CState) -> GResult<CState> {
         let selected_state = State::find_by_id(id).one(db).await;
         match selected_state {
             Ok(state_opt) => match state_opt {
@@ -317,7 +413,7 @@ impl ServiceMutation {
         }
     }
     // city
-    pub async fn create_city(db: &DbConn, data: CCity) -> Result<Uuid, DbErr> {
+    pub async fn create_city(db: &DbConn, data: CCity) -> CResult<Uuid> {
         let c_city = CityActiveModel {
             city_name: Set(data.name),
             state_id: Set(data.state_id),
@@ -328,7 +424,7 @@ impl ServiceMutation {
             Err(err) => return Err(err),
         }
     }
-    pub async fn delete_city(db: &DbConn, id: Uuid) -> Result<u64, String> {
+    pub async fn delete_city(db: &DbConn, id: Uuid) -> GResult<u64> {
         let selected_city = City::find_by_id(id).one(db).await;
         match selected_city {
             Ok(city_opt) => match city_opt {
@@ -341,7 +437,7 @@ impl ServiceMutation {
             Err(err) => Err(err.to_string()),
         }
     }
-    pub async fn update_city(db: &DbConn, id: Uuid, data: CCity) -> Result<CCity, String> {
+    pub async fn update_city(db: &DbConn, id: Uuid, data: CCity) -> GResult<CCity> {
         let selected_city = City::find_by_id(id).one(db).await;
         match selected_city {
             Ok(city_opt) => match city_opt {
