34c313295902db22fe5d6bf44c01b6214f92f1c4
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-12-01 16:15:55 +0000

    cleanup

--- src/routes/auth/mod.rs
+++ src/routes/auth/mod.rs
@@ -1,11 +1,10 @@
 // use crate::handlers::auth;
 use actix_web::{web, Scope};
 
-use crate::handlers::{auth, health_check};
+use crate::handlers::auth;
 
 pub fn load_auth_routes() -> Scope {
     web::scope("/auth")
         .route("/login", web::get().to(auth::login))
-        .route("/register", web::post().to(health_check::healthy))
         .route("/sessions/google", web::get().to(auth::google_auth_handler))
 }
