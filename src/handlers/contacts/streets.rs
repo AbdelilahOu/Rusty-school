b3d93e2335506b9d7df6d741477d9ad3443289fd
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-12-06 14:03:54 +0000

    FIX: turn error into a string

--- src/handlers/contacts/streets.rs
+++ src/handlers/contacts/streets.rs
@@ -52,7 +52,7 @@ pub async fn get_streets(queries: TQueries, body: TFiltersBody, state: State) ->
         Err(e) => HttpResponse::InternalServerError()
             .content_type(ContentType::json())
             .json(ResponseData::<Option<String>> {
-                error: Some(e),
+                error: Some(e.to_string()),
                 message: None,
                 data: None,
             }),
@@ -73,7 +73,7 @@ pub async fn delete_street(id: IdParam, state: State) -> HttpResponse {
         Err(e) => HttpResponse::InternalServerError()
             .content_type(ContentType::json())
             .json(ResponseData::<Option<String>> {
-                error: Some(e),
+                error: Some(e.to_string()),
                 message: None,
                 data: None,
             }),
@@ -94,7 +94,7 @@ pub async fn update_street(id: IdParam, body: CtBody, state: State) -> HttpRespo
         Err(e) => HttpResponse::InternalServerError()
             .content_type(ContentType::json())
             .json(ResponseData::<Option<String>> {
-                error: Some(e),
+                error: Some(e.to_string()),
                 message: None,
                 data: None,
             }),
