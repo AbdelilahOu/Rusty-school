6171527a76beef25d6581ae9c6fddfa5b41a9cb0
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-12-06 19:09:43 +0000

    FEAT: clean up routes

--- src/routes/scans.rs
+++ src/routes/scans.rs
@@ -1,11 +1,10 @@
 // use crate::handlers::scans;
 use actix_web::{web, Scope};
 
-use crate::handlers::{health_check, scans};
+use crate::handlers::scans;
 
 pub fn load_scans_routes() -> Scope {
     web::scope("/scans")
         .route("/", web::post().to(scans::create_scan))
         .route("/", web::get().to(scans::get_scans))
-        .route("/{id}", web::delete().to(health_check::healthy))
 }
