917230486db17049c535c44fb5a6a1ebd2086442
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-12-09 14:04:29 +0000

    group migrations

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,14 +1,10 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20231109_190937_c_student;
-mod m20231113_170500_c_teacher;
-mod m20231116_165911_c_parents;
-mod m20231116_171406_c_pickups;
-mod m20231116_214011_c_scans;
-mod m20231118_095513_c_details;
-mod m20231118_162555_c_person;
-mod m20231127_123039_c_user;
+mod school_related;
+mod user_related;
 
+use school_related::*;
+use user_related::*;
 pub struct Migrator;
 
 #[async_trait::async_trait]
---
+++ migration/src/user_related/mod.rs
@@ -0,0 +1,8 @@
+pub mod m20231109_190937_c_student;
+pub mod m20231113_170500_c_teacher;
+pub mod m20231116_165911_c_parents;
+pub mod m20231116_171406_c_pickups;
+pub mod m20231116_214011_c_scans;
+pub mod m20231118_095513_c_details;
+pub mod m20231118_162555_c_person;
+pub mod m20231127_123039_c_user;
