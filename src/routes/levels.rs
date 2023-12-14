a9143c9c5aa32e2e543dff51df9669452d8371d5
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-12-14 06:59:30 +0000

    FEAT: update routes level

--- src/routes/levels.rs
+++ src/routes/levels.rs
@@ -1,5 +1,11 @@
 use actix_web::{web, Scope};
 
+use crate::handlers::levels;
+
 pub fn load_levels_routes() -> Scope {
     web::scope("/levels")
+        .route("/filters", web::post().to(levels::list_levels))
+        .route("/", web::put().to(levels::update_level))
+        .route("/", web::post().to(levels::create_level))
+        .route("/", web::delete().to(levels::delete_level))
 }
