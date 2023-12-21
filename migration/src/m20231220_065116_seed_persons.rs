8cea09f1879389bc32d5d007cdc1963723b4c486
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-12-21 07:14:20 +0000

    TODO: finish seed person

--- migration/src/m20231220_065116_seed_persons.rs
+++ migration/src/m20231220_065116_seed_persons.rs
@@ -1,11 +1,42 @@
+use sea_orm::Statement;
 use sea_orm_migration::prelude::*;
+
+use crate::m20231118_095513_c_details::PersonDetails;
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
-    async fn up(&self, _: &SchemaManager) -> Result<(), DbErr> {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // let db = manager.get_connection();
+        for _ in 0..200 {
+            let sql = Query::select()
+                .from(PersonDetails::Table)
+                .column((Alias::new("person_details"), PersonDetails::Id))
+                .left_join(
+                    PersonDetails::Table,
+                    Expr::col((PersonDetails::Table, PersonDetails::Id))
+                        .equals((PersonDetails::Table, PersonDetails::Id)),
+                )
+                .conditions(
+                    true,
+                    |x| {
+                        x.and_where(Expr::col((PersonDetails::Table, PersonDetails::Id)).is_null());
+                    },
+                    |_| {},
+                )
+                .limit(2)
+                .to_owned()
+                .to_string(PostgresQueryBuilder);
+            println!("{}", sql);
+            // .build(PostgresQueryBuilder);
+            //
+            // let statment =
+            //     Statement::from_sql_and_values(sea_orm::DatabaseBackend::Postgres, sql, values);
+            // let rows = db.query_all(statment).await?;
+        }
+
         Ok(())
     }
 
