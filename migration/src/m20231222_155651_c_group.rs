c0a4a8f9c070a56d58b63d203151d4b644ffd0a5
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-12-23 09:14:15 +0000

    FEAT: create group migration

--- migration/src/m20231222_155651_c_group.rs
+++ migration/src/m20231222_155651_c_group.rs
@@ -6,10 +6,38 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Group::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Group::Id)
+                            .uuid()
+                            .not_null()
+                            .default(Expr::cust("gen_random_uuid()"))
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Group::Name).string().not_null())
+                    .col(ColumnDef::new(Group::Description).string())
+                    .col(ColumnDef::new(Group::LevelId).uuid())
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk_details_person_id")
+                            .from(Group::Table, Group::LevelId)
+                            .to(Level::Table, Level::Id)
+                            .on_delete(ForeignKeyAction::Cascade),
+                    )
+                    .to_owned(),
+            )
+            .await?;
         Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Group::Table).to_owned())
+            .await?;
         Ok(())
     }
 }
@@ -17,6 +45,19 @@ impl MigrationTrait for Migration {
 #[derive(DeriveIden)]
 enum Group {
     #[sea_orm(iden = "groups")]
+    Table,
+    Id,
+    #[sea_orm(iden = "group_name")]
+    Name,
+    #[sea_orm(iden = "level_id")]
+    LevelId,
+    #[sea_orm(iden = "group_description")]
+    Description,
+}
+
+#[derive(DeriveIden)]
+enum Level {
+    #[sea_orm(iden = "levels")]
     Table,
     Id,
 }
