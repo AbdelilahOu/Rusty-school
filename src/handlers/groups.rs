b00f395b943bd6f973c5ba6366eb665e4623c819
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-12-27 08:29:03 +0000

    FEAT: handlers

---
+++ src/handlers/groups.rs
@@ -0,0 +1,122 @@
+use crate::{guards::auth::check_token, models::commen::*};
+use actix_web::{
+    http::{header::ContentType, StatusCode},
+    web::Json as ActJson,
+    HttpRequest as HttpReq, HttpResponse as HttpRes,
+};
+use service::*;
+//
+type StBody = ActJson<CGroup>;
+
+pub async fn create_group(body: StBody, state: State, req: HttpReq) -> HttpRes {
+    let res = ServiceMutation::create_group(&state.db_conn, body.into_inner()).await;
+    match res {
+        Ok(id) => HttpRes::Ok()
+            .status(StatusCode::CREATED)
+            .content_type(ContentType::json())
+            .json(ResponseData {
+                error: None,
+                message: Some("Group created successfully".to_string()),
+                data: Some(id.to_string()),
+            }),
+        Err(e) => HttpRes::InternalServerError()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .content_type(ContentType::json())
+            .json(ResponseData::<Option<String>> {
+                error: Some(e.to_string()),
+                message: None,
+                data: None,
+            }),
+    }
+}
+
+pub async fn delete_group(id: IdParam, state: State) -> HttpRes {
+    let delete_res = ServiceMutation::delete_group(&state.db_conn, id.into_inner()).await;
+
+    match delete_res {
+        Ok(i) => HttpRes::Created()
+            .content_type(ContentType::json())
+            .json(ResponseData {
+                error: None,
+                message: Some("Group deleted successfully".to_string()),
+                data: Some(i.to_string()),
+            }),
+        Err(e) => HttpRes::InternalServerError()
+            .content_type(ContentType::json())
+            .json(ResponseData::<Option<String>> {
+                error: Some(e.to_string()),
+                message: None,
+                data: None,
+            }),
+    }
+}
+
+pub async fn get_level_groups(id: IdParam, state: State) -> HttpRes {
+    let selected_group = ServiceQuery::list_level_groups(&state.db_conn, id.into_inner()).await;
+
+    match selected_group {
+        Ok(i) => HttpRes::Created()
+            .content_type(ContentType::json())
+            .json(ResponseData {
+                error: None,
+                message: Some("Group selected by level id successfully".to_string()),
+                data: Some(i),
+            }),
+        Err(e) => HttpRes::InternalServerError()
+            .content_type(ContentType::json())
+            .json(ResponseData::<Option<String>> {
+                error: Some(e.to_string()),
+                message: None,
+                data: None,
+            }),
+    }
+}
+
+pub async fn list_groups(queries: TQueries, body: TFiltersBody, state: State) -> HttpRes {
+    let groups = ServiceQuery::list_groups(
+        &state.db_conn,
+        QueriesFilters {
+            queries: queries.into_inner(),
+            filters: body.clone().filters,
+        },
+    )
+    .await;
+
+    match groups {
+        Ok(i) => HttpRes::Created()
+            .content_type(ContentType::json())
+            .json(ResponseData {
+                error: None,
+                message: Some("Groups selected successfully".to_string()),
+                data: Some(i),
+            }),
+        Err(e) => HttpRes::InternalServerError()
+            .content_type(ContentType::json())
+            .json(ResponseData::<Option<String>> {
+                error: Some(e.to_string()),
+                message: None,
+                data: None,
+            }),
+    }
+}
+
+pub async fn update_group(id: IdParam, body: StBody, state: State) -> HttpRes {
+    let update_res =
+        ServiceMutation::update_group(&state.db_conn, id.into_inner(), body.into_inner()).await;
+    match update_res {
+        Ok(i) => HttpRes::Created()
+            .content_type(ContentType::json())
+            .json(ResponseData {
+                error: None,
+                message: Some("Group updated successfully".to_string()),
+                data: Some(i),
+            }),
+        Err(e) => HttpRes::InternalServerError()
+            .content_type(ContentType::json())
+            .json(ResponseData::<Option<String>> {
+                error: Some(e.to_string()),
+                message: None,
+                data: None,
+            }),
+    }
+}
