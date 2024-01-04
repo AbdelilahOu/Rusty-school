eb1c276f5ca61a14d44659b98fc2b624dc37a1cf
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-01-04 15:04:23 +0000

    FEAT: update filters

--- service/src/utils/filters.rs
+++ service/src/utils/filters.rs
@@ -9,25 +9,8 @@ pub fn generate_student_filters(filters: Vec<Filters>) -> Condition {
         .into_iter()
         .fold(Condition::all(), |conditions, filter| {
             match filter.feild.as_str() {
-                "full_name" => match filter.operation.as_str() {
-                    "LIKE" => conditions.add(StudentCol::FullName.like(filter.value)),
-                    _ => conditions,
-                },
-                "last_name" => match filter.operation.as_str() {
-                    "LIKE" => conditions.add(StudentCol::LastName.like(filter.value)),
-                    _ => conditions,
-                },
-                "first_name" => match filter.operation.as_str() {
-                    "LIKE" => conditions.add(StudentCol::FirstName.like(filter.value)),
-                    _ => conditions,
-                },
-                "level" => match filter.operation.as_str() {
-                    "=" => {
-                        // get level id using level
-                        conditions
-                    }
-                    _ => conditions,
-                },
+                "full_name" => conditions.add(StudentCol::FullName.contains(filter.value)),
+                "group_id" => conditions.add(StudentCol::GroupId.eq(filter.value)),
                 _ => conditions,
             }
         })
@@ -38,18 +21,7 @@ pub fn generate_teacher_filters(filters: Vec<Filters>) -> Condition {
         .into_iter()
         .fold(Condition::all(), |conditions, filter| {
             match filter.feild.as_str() {
-                "full_name" => match filter.operation.as_str() {
-                    "LIKE" => conditions.add(TeacherCol::FullName.like(filter.value)),
-                    _ => conditions,
-                },
-                "last_name" => match filter.operation.as_str() {
-                    "LIKE" => conditions.add(TeacherCol::LastName.like(filter.value)),
-                    _ => conditions,
-                },
-                "first_name" => match filter.operation.as_str() {
-                    "LIKE" => conditions.add(TeacherCol::FirstName.like(filter.value)),
-                    _ => conditions,
-                },
+                "full_name" => conditions.add(TeacherCol::FullName.contains(filter.value)),
                 _ => conditions,
             }
         })
@@ -60,18 +32,7 @@ pub fn generate_parent_filters(filters: Vec<Filters>) -> Condition {
         .into_iter()
         .fold(Condition::all(), |conditions, filter| {
             match filter.feild.as_str() {
-                "full_name" => match filter.operation.as_str() {
-                    "LIKE" => conditions.add(ParentCol::FullName.like(filter.value)),
-                    _ => conditions,
-                },
-                "last_name" => match filter.operation.as_str() {
-                    "LIKE" => conditions.add(ParentCol::LastName.like(filter.value)),
-                    _ => conditions,
-                },
-                "first_name" => match filter.operation.as_str() {
-                    "LIKE" => conditions.add(ParentCol::FirstName.like(filter.value)),
-                    _ => conditions,
-                },
+                "full_name" => conditions.add(ParentCol::FullName.contains(filter.value)),
                 _ => conditions,
             }
         })
