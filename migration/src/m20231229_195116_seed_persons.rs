5c6dc4c008a86d70fd89b67a841ccb80d702b0fd
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-01-07 08:09:25 +0000

    FEAT: rewrite persons seed migrations

--- migration/src/m20231229_195116_seed_persons.rs
+++ migration/src/m20231229_195116_seed_persons.rs
@@ -3,8 +3,7 @@ use sea_orm_migration::prelude::*;
 
 use crate::{
     m20231109_190937_c_students::Student, m20231113_170500_c_teachers::Teacher,
-    m20231116_165911_c_parents::Parent, m20231118_095513_c_details::PersonDetails,
-    m20231118_162555_c_persons::Person,
+    m20231116_165911_c_parents::Parent, m20231118_162555_c_persons::Person,
 };
 
 #[derive(DeriveMigrationName)]
@@ -13,209 +12,161 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        //
         let db = manager.get_connection();
-        //
-        let (details_sql, details_values) = Query::select()
-            .from(PersonDetails::Table)
-            .column((Alias::new("person_details"), PersonDetails::Id))
-            .left_join(
-                Person::Table,
-                Expr::col((Person::Table, Person::DetailsId))
-                    .equals((PersonDetails::Table, PersonDetails::Id)),
-            )
-            .conditions(
-                true,
-                |x| {
-                    x.and_where(Expr::col((Person::Table, Person::DetailsId)).is_null());
-                },
-                |_| {},
-            )
-            .limit(1)
-            .to_owned()
-            .build(PostgresQueryBuilder);
-
-        let (parent_sql, parent_values) = Query::select()
-            .from(Parent::Table)
-            .column((Alias::new("parents"), Parent::Id))
-            .left_join(
-                Person::Table,
-                Expr::col((Person::Table, Person::Id)).equals((Parent::Table, Parent::PersonId)),
-            )
-            .conditions(
-                true,
-                |x| {
-                    x.and_where(Expr::col((Parent::Table, Parent::PersonId)).is_null());
-                },
-                |_| {},
-            )
-            .limit(1)
-            .to_owned()
-            .build(PostgresQueryBuilder);
-
-        let (student_sql, student_values) = Query::select()
-            .from(Student::Table)
-            .column((Alias::new("students"), Student::Id))
-            .left_join(
-                Person::Table,
-                Expr::col((Person::Table, Person::Id)).equals((Student::Table, Student::PersonId)),
-            )
-            .conditions(
-                true,
-                |x| {
-                    x.and_where(Expr::col((Student::Table, Student::PersonId)).is_null());
-                },
-                |_| {},
-            )
-            .limit(1)
-            .to_owned()
-            .build(PostgresQueryBuilder);
-
-        let (teacher_sql, teacher_values) = Query::select()
-            .from(Teacher::Table)
-            .column((Alias::new("teachers"), Teacher::Id))
-            .left_join(
-                Person::Table,
-                Expr::col((Person::Table, Person::Id)).equals((Teacher::Table, Teacher::PersonId)),
-            )
-            .conditions(
-                true,
-                |x| {
-                    x.and_where(Expr::col((Teacher::Table, Teacher::PersonId)).is_null());
-                },
-                |_| {},
-            )
-            .limit(1)
-            .to_owned()
-            .build(PostgresQueryBuilder);
-
         for _ in 0..200 {
-            // SELECT DETAILS ID
-            let statment = Statement::from_sql_and_values(
+            let get_person_id = Statement::from_string(
                 sea_orm::DatabaseBackend::Postgres,
-                details_sql.clone(),
-                details_values.clone(),
+                r#"
+                INSERT INTO
+                    persons (person_type, details_id)
+                VALUES
+                    (
+                        'student',(
+                            SELECT
+                                pd.id
+                            FROM
+                                person_details as pd
+                            WHERE
+                                pd.id not in (
+                                    SELECT
+                                        p.details_id
+                                    from
+                                        persons as p
+                                )
+                            order by
+                                random()
+                            limit
+                                1
+                        )
+                    ) RETURNING id"#,
             );
-            let details_row = db.query_one(statment).await?;
-            let details_id = details_row.unwrap().try_get::<Uuid>("", "id").unwrap();
-            // SELECT STUDENT ID
-            let statment = Statement::from_sql_and_values(
-                sea_orm::DatabaseBackend::Postgres,
-                student_sql.clone(),
-                student_values.clone(),
-            );
-            let student_row = db.query_one(statment).await?;
-            let student_id = student_row.unwrap().try_get::<Uuid>("", "id").unwrap();
-            // get person type
-            let person_type = String::from("student");
-            let (sql, values) = Query::insert()
-                .into_table(Person::Table)
-                .columns([Person::PersonType, Person::DetailsId])
-                .values_panic([person_type.clone().into(), details_id.clone().into()])
-                .returning(ReturningClause::Columns(vec![ColumnRef::Column(
-                    SeaRc::new(Person::Id),
-                )]))
-                .build(PostgresQueryBuilder);
-
-            let statment =
-                Statement::from_sql_and_values(sea_orm::DatabaseBackend::Postgres, sql, values);
-
-            let row = db.query_one(statment).await?;
+            let row = db.query_one(get_person_id).await?;
             let person_id = row.unwrap().try_get::<Uuid>("", "id").unwrap();
-
-            let update_student = Query::update()
-                .table(Student::Table)
-                .values([(Student::PersonId, person_id.clone().into())])
-                .and_where(Expr::col((Student::Table, Student::Id)).eq(student_id))
-                .to_owned();
-
-            manager.exec_stmt(update_student).await?;
+            let update_student_person_id = Statement::from_sql_and_values(
+                sea_orm::DatabaseBackend::Postgres,
+                r#"
+                UPDATE
+                    students
+                SET
+                    person_id = $1
+                WHERE
+                    id IN (
+                        SELECT
+                            s.id
+                        FROM
+                            students as s
+                        WHERE
+                            s.person_id IS NULL 
+                        LIMIT 1
+                    );
+                "#,
+                [person_id.into()],
+            );
+            db.execute(update_student_person_id).await?;
         }
 
         for _ in 0..100 {
-            // SELECT DETAILS ID
-            let statment = Statement::from_sql_and_values(
+            let get_person_id = Statement::from_string(
                 sea_orm::DatabaseBackend::Postgres,
-                details_sql.clone(),
-                details_values.clone(),
+                r#"
+                INSERT INTO
+                    persons (person_type, details_id)
+                VALUES
+                    (
+                        'parent',(
+                            SELECT
+                                pd.id
+                            FROM
+                                person_details as pd
+                            WHERE
+                                pd.id not in (
+                                    SELECT
+                                        p.details_id
+                                    from
+                                        persons as p
+                                )
+                            order by
+                                random()
+                            limit
+                                1
+                        )
+                    ) RETURNING id"#,
             );
-            let details_row = db.query_one(statment).await?;
-            let details_id = details_row.unwrap().try_get::<Uuid>("", "id").unwrap();
-            // SELECT pARENT ID
-            let statment = Statement::from_sql_and_values(
-                sea_orm::DatabaseBackend::Postgres,
-                parent_sql.clone(),
-                parent_values.clone(),
-            );
-            let parent_row = db.query_one(statment).await?;
-            let parent_id = parent_row.unwrap().try_get::<Uuid>("", "id").unwrap();
-            //
-            let person_type = String::from("parent");
-            let (sql, values) = Query::insert()
-                .into_table(Person::Table)
-                .columns([Person::PersonType, Person::DetailsId])
-                .values_panic([person_type.clone().into(), details_id.clone().into()])
-                .returning(ReturningClause::Columns(vec![ColumnRef::Column(
-                    SeaRc::new(Person::Id),
-                )]))
-                .build(PostgresQueryBuilder);
-
-            let statment =
-                Statement::from_sql_and_values(sea_orm::DatabaseBackend::Postgres, sql, values);
-
-            let row = db.query_one(statment).await?;
+            let row = db.query_one(get_person_id).await?;
             let person_id = row.unwrap().try_get::<Uuid>("", "id").unwrap();
-
-            let update_parent = Query::update()
-                .table(Parent::Table)
-                .values([(Parent::PersonId, person_id.clone().into())])
-                .and_where(Expr::col((Parent::Table, Parent::Id)).eq(parent_id))
-                .to_owned();
-
-            manager.exec_stmt(update_parent).await?;
+            let update_parent_person_id = Statement::from_sql_and_values(
+                sea_orm::DatabaseBackend::Postgres,
+                r#"
+                UPDATE
+                    parents
+                SET
+                    person_id = $1
+                WHERE
+                    id = (
+                        SELECT
+                            id
+                        FROM
+                            parents
+                        WHERE
+                            person_id IS NULL 
+                        LIMIT 1
+                    );
+                "#,
+                [person_id.into()],
+            );
+            db.execute(update_parent_person_id).await?;
         }
 
         for _ in 0..50 {
-            // SELECT DETAILS ID
-            let statment = Statement::from_sql_and_values(
+            let get_person_id = Statement::from_string(
                 sea_orm::DatabaseBackend::Postgres,
-                details_sql.clone(),
-                details_values.clone(),
+                r#"
+                INSERT INTO
+                    persons (person_type, details_id)
+                VALUES
+                    (
+                        'teacher',(
+                            SELECT
+                                pd.id
+                            FROM
+                                person_details as pd
+                            WHERE
+                                pd.id not in (
+                                    SELECT
+                                        p.details_id
+                                    from
+                                        persons as p
+                                )
+                            order by
+                                random()
+                            limit
+                                1
+                        )
+                    ) RETURNING id"#,
             );
-            let details_row = db.query_one(statment).await?;
-            let details_id = details_row.unwrap().try_get::<Uuid>("", "id").unwrap();
-            // SELECT TEACHER ID
-            let statment = Statement::from_sql_and_values(
-                sea_orm::DatabaseBackend::Postgres,
-                teacher_sql.clone(),
-                teacher_values.clone(),
-            );
-            let teacher_row = db.query_one(statment).await?;
-            let teacher_id = teacher_row.unwrap().try_get::<Uuid>("", "id").unwrap();
-            //
-            let person_type = String::from("teacher");
-            let (sql, values) = Query::insert()
-                .into_table(Person::Table)
-                .columns([Person::PersonType, Person::DetailsId])
-                .values_panic([person_type.clone().into(), details_id.clone().into()])
-                .returning(ReturningClause::Columns(vec![ColumnRef::Column(
-                    SeaRc::new(Person::Id),
-                )]))
-                .build(PostgresQueryBuilder);
-
-            let statment =
-                Statement::from_sql_and_values(sea_orm::DatabaseBackend::Postgres, sql, values);
-
-            let row = db.query_one(statment).await?;
+            let row = db.query_one(get_person_id).await?;
             let person_id = row.unwrap().try_get::<Uuid>("", "id").unwrap();
-
-            let update_teacher = Query::update()
-                .table(Teacher::Table)
-                .values([(Teacher::PersonId, person_id.clone().into())])
-                .and_where(Expr::col((Teacher::Table, Teacher::Id)).eq(teacher_id))
-                .to_owned();
-
-            manager.exec_stmt(update_teacher).await?;
+            let update_teacher_person_id = Statement::from_sql_and_values(
+                sea_orm::DatabaseBackend::Postgres,
+                r#"
+                UPDATE
+                    teachers
+                SET
+                    person_id = $1
+                WHERE id = 
+                    (
+                        SELECT
+                            id
+                        FROM
+                            teachers
+                        WHERE
+                            person_id IS NULL 
+                        LIMIT 1
+                    );
+                "#,
+                [person_id.into()],
+            );
+            db.execute(update_teacher_person_id).await?;
         }
 
         Ok(())
