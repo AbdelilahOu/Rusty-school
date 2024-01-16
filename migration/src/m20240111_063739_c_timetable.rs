843b93dd09d082e355e88a9bfcdf36233fcbbe74
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-01-16 12:05:53 +0000

    FEAT: time table migration

--- migration/src/m20240111_063739_c_timetable.rs
+++ migration/src/m20240111_063739_c_timetable.rs
@@ -6,6 +6,29 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager.create_table(
+            Table::create()
+                .table(TimeTable::Table)
+                .if_not_exists()
+                .col(
+                    ColumnDef::new(TimeTable::Id)
+                        .uuid()
+                        .not_null()
+                        .default(Expr::cust("gen_random_uuid()"))
+                        .primary_key(),
+                )
+                .col(ColumnDef::new(TimeTable::ClassId).uuid())
+                .foreign_key(
+                    ForeignKey::create()
+                        .name("fk_time_table_class_id")
+                        .from(TimeTable::Table, TimeTable::ClassId)
+                        .to(Class::Table, Class::Id)
+                        .on_delete(ForeignKeyAction::SetNull),
+                )
+                .col(ColumnDef::new(TimeTable::DayOfWeek).time())
+                .col(ColumnDef::new(TimeTable::StartTime).time())
+                .col(ColumnDef::new(TimeTable::EndTime).text()),
+        );
         Ok(())
     }
 
@@ -19,4 +42,12 @@ enum TimeTable {
     #[sea_orm(iden = "time_table")]
     Table,
     Id,
+    #[sea_orm(iden = "day_of_week")]
+    DayOfWeek,
+    #[sea_orm(iden = "start_time")]
+    StartTime,
+    #[sea_orm(iden = "end_time")]
+    EndTime,
+    #[sea_orm(iden = "class_id")]
+    ClassId,
 }
