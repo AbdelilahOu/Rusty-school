12f171eda39f5045d621575d1d0aabcfef5b701d
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-01-31 19:23:16 +0000

    TODO: seed time table migration

--- migration/src/m20240124_092916_seed_time_table.rs
+++ migration/src/m20240124_092916_seed_time_table.rs
@@ -5,13 +5,11 @@ pub struct Migration;
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
-    async fn up(&self, _: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
     }
 
-    async fn down(&self, _: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
     }
 }
