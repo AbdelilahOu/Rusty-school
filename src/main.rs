44b32fab3e75a8bfab159a55384c40e2b4d03dc8
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-02-05 12:10:52 +0000

    FEAT: Test with neon db

--- src/main.rs
+++ src/main.rs
@@ -59,7 +59,7 @@ async fn main() -> std::io::Result<()> {
             .service(load_timetable_routes())
             .default_service(web::to(|| HttpResponse::NotFound()))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await;
     Ok(())
