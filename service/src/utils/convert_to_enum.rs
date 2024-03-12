a6a5717dac09ff5ca4205a9d2c8afb7665bd65b6
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-03-12 13:29:27 +0000

    FEAT: update days of the week

--- service/src/utils/convert_to_enum.rs
+++ service/src/utils/convert_to_enum.rs
@@ -2,14 +2,13 @@ use entity::sea_orm_active_enums::DayOfWeekEnum;
 
 pub fn to_day_of_week(i: u32) -> Option<DayOfWeekEnum> {
     match i {
-        0 => None,
+        0 => Some(DayOfWeekEnum::Sunday),
         1 => Some(DayOfWeekEnum::Monday),
         2 => Some(DayOfWeekEnum::Tuesday),
         3 => Some(DayOfWeekEnum::Wednesday),
         4 => Some(DayOfWeekEnum::Thursday),
         5 => Some(DayOfWeekEnum::Friday),
         6 => Some(DayOfWeekEnum::Saturday),
-        7 => Some(DayOfWeekEnum::Sunday),
         _ => None,
     }
 }
