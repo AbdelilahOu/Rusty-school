b42a8b500098b3cd0aa4fe68372e6359d6dc9ffd
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-04-19 11:03:08 +0000

    FEAT: handle db connection error

--- src/main.rs
+++ src/main.rs
@@ -22,41 +22,42 @@ async fn main() -> std::io::Result<()> {
     // load config
     let loaded_config = config::load_config();
     //
-    let conn = database::establish_connection(loaded_config.db_url.clone())
-        .await
-        .unwrap();
-    //
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-    // start server
-    println!("Server started on http://0.0.0.0:8080");
-    let _ = HttpServer::new(move || {
-        App::new()
-            .wrap(Logger::new("> %r status: [%s] took: %T s"))
-            .app_data(web::Data::new(AppState {
-                db_conn: conn.clone(),
-                config: loaded_config.clone(),
-            }))
-            .service(auth::load_auth_routes())
-            .service(people::load_students_routes())
-            .service(people::load_teachers_routes())
-            .service(people::load_parents_routes())
-            .service(academic::load_levels_routes())
-            .service(academic::load_scans_routes())
-            .service(academic::load_subjects_routes())
-            .service(academic::load_groups_routes())
-            .service(academic::load_rooms_routes())
-            .service(academic::load_classes_routes())
-            .service(academic::load_attendance_routes())
-            .service(academic::load_timetable_routes())
-            .service(academic::load_assignments_routes())
-            .service(academic::load_grades_routes())
-            .service(academic::load_grading_rubric_routes())
-            .service(academic::load_disciplinary_actions_routes())
-            .service(communication::load_announcements_routes())
-            .default_service(web::to(|| Response::NotFound()))
-    })
-    .bind(("0.0.0.0", 8080))?
-    .run()
-    .await;
+    match database::establish_connection(loaded_config.db_url.clone()).await {
+        Ok(conn) => {
+            env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+            // start server
+            println!("Server started on http://0.0.0.0:8080");
+            let _ = HttpServer::new(move || {
+                App::new()
+                    .wrap(Logger::new("> %r status: [%s] took: %T s"))
+                    .app_data(web::Data::new(AppState {
+                        db_conn: conn.clone(),
+                        config: loaded_config.clone(),
+                    }))
+                    .service(auth::load_auth_routes())
+                    .service(people::load_students_routes())
+                    .service(people::load_teachers_routes())
+                    .service(people::load_parents_routes())
+                    .service(academic::load_levels_routes())
+                    .service(academic::load_scans_routes())
+                    .service(academic::load_subjects_routes())
+                    .service(academic::load_groups_routes())
+                    .service(academic::load_rooms_routes())
+                    .service(academic::load_classes_routes())
+                    .service(academic::load_attendance_routes())
+                    .service(academic::load_timetable_routes())
+                    .service(academic::load_assignments_routes())
+                    .service(academic::load_grades_routes())
+                    .service(academic::load_grading_rubric_routes())
+                    .service(academic::load_disciplinary_actions_routes())
+                    .service(communication::load_announcements_routes())
+                    .default_service(web::to(|| Response::NotFound()))
+            })
+            .bind(("0.0.0.0", 8080))?
+            .run()
+            .await;
+        }
+        Err(e) => println!("establish db connection failed: {}", e),
+    }
     Ok(())
 }
